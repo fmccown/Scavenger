24b2dcc3e33d528a94b88d136844740a1778a685
Frank McCown <fmccown@harding.edu>
2020-11-09 14:40:03 -0600

    Add project

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (GameManager.instance == null)
+        {
+            Debug.Log("Loader: GameManager.instance is null");
+
+            Instantiate(gameManager);
+        }
+    }
+}
